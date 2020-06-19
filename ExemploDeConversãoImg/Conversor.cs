38792b0ab51153fdfd47844d44c729d580572173
DiogoBotton <diogo.luo@gmail.com>
2020-06-19 17:28:32 -0300

    add fonte

--- ExemploDeConversãoImg/Conversor.cs
+++ ExemploDeConversãoImg/Conversor.cs
@@ -17,3 +17,5 @@ public class Conversor
         }
     }
 }
+
+// Fonte: https://joaoretamero.com.br/salvando-imagens-csharp-entity/#:~:text=Preparando%20o%20modelo,usar%20um%20array%20de%20byte.&text=Neste%20caso%20o%20Entity%20j%C3%A1,a%20coluna%20na%20tabela%20corretamente.
